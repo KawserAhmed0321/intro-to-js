fb49db4e45c1be3e8f8c5c55584dd983f9a5d8b9
KawserAhmed0321 <kanonmmc@gmail.com>
2022-01-27 14:05:43 +0600

    first commit

---
+++ first.js
@@ -0,0 +1,21 @@
+var age = 21;
+var temp = 23;
+var id = 21;
+console.log(age, temp, id);
+var price1 = 21;
+var price2 = 23;
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+price1 = price1 + 10;
+
+price1 += 10;
+console.log(price1);
+price1++;
+console.log(price1);
+var num1 = '10.56';
+var num2 = '21.87';
+var num = parseInt(num1);
+console.log(num);
+var fruits = ['apple', 'banana'];
+console.log(fruits.length);
+
---
+++ variable.js
@@ -0,0 +1,7 @@
+// num trype 
+var num = [120, 130, 140];
+console.log(num);
+
+// string type 
+var name = ['anita', 'ganita', 'bannita', 'sanita'];
+console.log(name);
