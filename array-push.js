6f4bf8ac174775de9fcb7bf347e1d88d168c0b99
KawserAhmed0321 <kanonmmc@gmail.com>
2022-01-27 14:22:49 +0600

    array -push added

---
+++ array-push.js
@@ -0,0 +1,11 @@
+// array push {add element array lasted}
+var ar = [1, 2, 3, 4, 5];
+console.log(ar);
+ar.push(6);
+console.log(ar)
+
+//array unshift {add element array first}
+var name = ['anita', 'ganita', 'banita'];
+console.log(name);
+name.unshift("fanita");
+console.log(name);
