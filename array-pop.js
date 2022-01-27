93b43e7b0d81288f96ff4e40b20ea06380caf34c
KawserAhmed0321 <kanonmmc@gmail.com>
2022-01-27 14:27:41 +0600

    add array-pop

---
+++ array-pop.js
@@ -0,0 +1,11 @@
+// array pop element remove last element 
+var num = [1, 2, 3, 4, 5, 6];
+console.log(num);
+num.pop();
+console.log(num);
+
+// array shift element remove 1st element 
+var name = ['anita', 'ganita', 'banita', 'fanita'];
+console.log(name);
+name.shift();
+console.log(name);
