6c3c351f9fb04714f3b63985e4bd7ada3e952346
KawserAhmed0321 <kanonmmc@gmail.com>
2022-10-01 01:01:17 +0600

    add if-else

---
+++ if-else.js
@@ -0,0 +1,13 @@
+var mybuget = 500;
+var price =600;
+var poket = true;
+var gotjob = true;
+
+
+if (mybuget > price && poket == true && gotjob == true)
+{
+    console.log("can't buy");
+}
+else{
+    console.log('buy ');
+}
