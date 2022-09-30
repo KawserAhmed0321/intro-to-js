8409b29f2cb112dd8d04918905484eda19e895b5
KawserAhmed0321 <kanonmmc@gmail.com>
2022-09-30 23:57:50 +0600

    add multi

--- math.js
+++ math.js
@@ -1,6 +1,8 @@
 var price = 21;
 var eggprice = 20;
 var totalPrice = price + eggprice ;
+var differcncePrice = price - eggprice ;
+var multiPrice = price * eggprice ;
 
 
 
@@ -8,3 +10,5 @@ console.log(price);
 console.log(eggprice);
 console.log(price + eggprice);
 console.log(totalPrice);
+console.log(differcncePrice);
+console.log(multiPrice);
