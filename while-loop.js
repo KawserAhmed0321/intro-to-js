d1125f8bd75f29f84e5adf9eaf07bd8a1b63b339
KawserAhmed0321 <kanonmmc@gmail.com>
2022-10-01 01:25:23 +0600

    add while-loop even &odd

--- while-loop.js
+++ while-loop.js
@@ -1,6 +1,22 @@
-var num = 0;
-while( num < 10)
-{
+var num = 1;
+while (num <= 10) {
     console.log(num);
-    num ++;
+    num++;
+}
+
+//even number 
+console.log('even number');
+var n = 1;
+while (n <= 20) {
+    console.log(n);
+    n = n + 2;
+}
+
+//odd number 
+console.log('odd number');
+
+var n1 = 0;
+while (n1 <= 20) {
+    console.log(n1);
+    n1 += 2;
 }
