e679908b630fdc7f76c9cdcab95f168e23bbc7e6
KawserAhmed0321 <kanonmmc@gmail.com>
2022-10-01 00:15:28 +0600

    add conversion

---
+++ conversion.js
@@ -0,0 +1,19 @@
+var firstName = "anita";
+var lastName = "khatun";
+var fullName = firstName + lastName ;
+var fullName2 =firstName + ' ' + lastName;
+
+
+console.log(fullName);
+console.log(fullName2);
+
+
+var eggPrice ='45.67';
+var onnionPrice ="34.87";
+var eggPriceNum = parseInt(eggPrice);
+var onnionPriceNum = parseFloat(onnionPrice);
+
+console.log(eggPrice);
+console.log(onnionPrice);
+console.log(eggPriceNum);
+console.log(onnionPriceNum);
